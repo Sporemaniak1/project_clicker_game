ad26ade71b5e410902cb333ab5a7d5d6c0af7237
DESKTOP-251C3PC\karol <karol6n@gmail.com>
2020-11-27 01:36:23 +0100

    Dodano globalne zasoby

---
+++ Clicker Game/Assets/Scripts/GameScene/GlobalResources.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalResources : MonoBehaviour
+{
+    public static int ResourceCount;
+    public GameObject ResourceDisplay;
+    public int InternalResources;
+
+    void Update()
+    {
+        InternalResources = ResourceCount;
+        ResourceDisplay.GetComponent<Text>().text = "Resources: " + InternalResources;
+    }
+}
