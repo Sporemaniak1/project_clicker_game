43cfac40dfdeb65b7c9502d8b621d6809e5b2c02
DESKTOP-251C3PC\karol <karol6n@gmail.com>
2020-11-27 18:36:27 +0100

    Dodano Auto Resource

---
+++ Clicker Game/Assets/Scripts/GameScene/AutoResource.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoResource : MonoBehaviour
+{
+    public bool CreatingResource = false;
+    public static int ResourceIncrease = 1;
+    public int InternalIncrease;
+   
+    void Update()
+    {
+        InternalIncrease = ResourceIncrease;
+        if (CreatingResource == false)
+        {
+            CreatingResource = true;
+            StartCoroutine(CreateTheResource());
+        }
+    }
+
+    IEnumerator CreateTheResource ()
+    {
+        GlobalResources.ResourceCount += InternalIncrease;
+        yield return new WaitForSeconds(1);
+        CreatingResource = false;
+    }
+}
