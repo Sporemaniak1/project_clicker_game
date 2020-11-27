8c57dfd612dc289702d3461d0bed2dd01fcc597a
DESKTOP-251C3PC\karol <karol6n@gmail.com>
2020-11-27 01:38:07 +0100

    Dodano pieniądz, możliwość sprzedawania surowców

--- Clicker Game/Assets/Scripts/GameScene/SellResource.cs
+++ Clicker Game/Assets/Scripts/GameScene/SellResource.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainButtonClick : MonoBehaviour
+public class SellResource : MonoBehaviour
 {
     public GameObject textBox;
 
     public void ClickTheButton ()
     {
-        GlobalResources.ResourceCount += 1;
+        GlobalResources.ResourceCount -= 1;
+        GlobalCash.CashCount += 1;
     }
 }
