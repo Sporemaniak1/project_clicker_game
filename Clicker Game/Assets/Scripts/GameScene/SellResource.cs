a102261156f7791c87f58558b5aaa062904fb13d
DESKTOP-251C3PC\karol <karol6n@gmail.com>
2020-11-27 17:23:25 +0100

    Dodano StatusBox który informuje gracza o braku zasobów oraz animację tego tekstu

--- Clicker Game/Assets/Scripts/GameScene/SellResource.cs
+++ Clicker Game/Assets/Scripts/GameScene/SellResource.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SellResource : MonoBehaviour
 {
     public GameObject textBox;
-
+    public GameObject statusBox;
     public void ClickTheButton ()
     {
-        GlobalResources.ResourceCount -= 1;
-        GlobalCash.CashCount += 1;
+        if (GlobalResources.ResourceCount == 0)
+        {
+            statusBox.GetComponent<Text>().text = "Not enough resources to sell. ";
+            statusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else
+        {
+            GlobalResources.ResourceCount -= 1;
+            GlobalCash.CashCount += 1;
+        }
+       
     }
 }
