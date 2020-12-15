8161443b67f2c9cb32e723d33d8fa3beaa65ca52
Robert_L_ <rrr23102000@gmail.com>
2020-12-15 15:12:23 +0100

    v 0.2.8 wstępne dźwięki

---
+++ Clicker Game/Assets/SoundEffects/SFXPlaying.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlaying : MonoBehaviour
+{
+    
+    public AudioSource MineButton;
+    public AudioSource SellButton;
+
+
+    public void PlayMineButton()
+    {
+        MineButton.Play();
+    }
+
+    public void PlaySellButton()
+    {
+        SellButton.Play();
+    }
+}
