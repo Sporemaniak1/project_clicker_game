59f14dfa7a22d37f27b69242ecf16987b8e95f19
Sporemaniak1 <59826178+Sporemaniak1@users.noreply.github.com>
2021-01-08 20:53:44 +0100

    Wstępna implementacja zmiany języka
    Co-Authored-By: RLaszczak <73471695+RLaszczak@users.noreply.github.com>
    Co-Authored-By: Karol6n <73205212+Karol6n@users.noreply.github.com>

---
+++ Clicker Game/Assets/Language/LanguageSelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageSelect : MonoBehaviour
+{
+    List<string> names = new List<string>() { "Select Language", "English", "Polski" };
+
+    public Dropdown LangList;
+    public Text SelectedLanguage;
+
+    public void DropdownIndexChanges(int index)
+    {
+        SelectedLanguage.text = names[index];
+    }
+
+    void Start()
+    {
+        SetLanguage();
+    }
+
+    void SetLanguage()
+    {
+        LangList.AddOptions(names);
+    }
+
+    
+}
