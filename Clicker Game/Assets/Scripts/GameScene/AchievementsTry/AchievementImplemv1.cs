7409f0923ccd3c8cf273d874596375535a1e6a0b
Robert_L_ <rrr23102000@gmail.com>
2021-01-24 17:32:10 +0100

    Dodanie kodu potrzebnego do osiągnięć

---
+++ Clicker Game/Assets/Scripts/GameScene/AchievementsTry/AchievementImplemv1.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementMenager : MonoBehaviour
+{
+    [System.Serializable]
+    public class AchievementMen //achievementMenagerInstance 
+    { 
+    }
+    
+    private void Awake()
+    {
+   // achievementMenagerInstance = this;
+    }
+
+    void Start()
+    {
+
+    }
+}
