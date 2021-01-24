5cb6f3ab61dec7102ca421ca9999e55b74d88cc9
Robert_L_ <rrr23102000@gmail.com>
2021-01-24 17:22:22 +0100

    Dodawanie kodu

---
+++ Clicker Game/Assets/Scripts/GameScene/AchievementsTry/ClickBtn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickBtn : MonoBehaviour
+{
+    public Text clickDisplay;
+    public int clicks;
+
+    public void Click()
+    {
+        clicks++;
+        clickDisplay.text = clicks + "CLICKS";
+
+    }
+}
+
